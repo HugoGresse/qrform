b26d4210aeeee18dc1dd9b43c0a00d06cf2f1e32
Hugo Gresse <hugo.gresse@gmail.com>
2022-09-28 20:44:15 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<main>
+  <p>
+    QRFOrm.fr fournit une solution de formulaire pour les pharmaciens, en respect avec les données des patients, hébergé en France, sans suivit utilisateur.
+  </p>
+
+</main>
