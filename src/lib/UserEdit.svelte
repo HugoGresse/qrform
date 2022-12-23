6e9ffb604b7355b016d1ae825bc5cd3bd04bd06e
Hugo Gresse <hugo.gresse@gmail.com>
2022-12-23 17:56:29 +0100

    Display form list

--- src/lib/UserEdit.svelte
+++ src/lib/UserEdit.svelte
@@ -1,14 +1,75 @@
 <script>
+    import { getLoginTokenCookie } from '../utils/cookie.js'
+    import CircularProgress from '@smui/circular-progress'
+    import FormItem from './form/FormItem.svelte'
 
     const GET_URL = import.meta.env.VITE_USER_FORM_GET
     const UPDATE_URL = import.meta.env.VITE_USER_FORM_UPDATE
 
+    let loading = false
+    let error = null
+    let forms = []
+
+
+    const load = async () => {
+        loading = true
+
+        const headers = new Headers()
+        headers.append("Content-Type", "application/json")
+
+        const response = await fetch(GET_URL, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({
+                token: getLoginTokenCookie(),
+            })
+        })
+        if (response.ok) {
+            const json = await response.json()
+            if(Array.isArray(json) && json.length > 0){
+                forms=json
+            } else {
+                error= "Erreur lors de la récupération des fomulaires, connexion expiré ?"
+            }
+        } else {
+            error = "Erreur lors de la récupération des formulaires"
+        }
+
+
+        loading = false
+
+    }
+
+    load()
 </script>
 
 
 
 <main>
+    <img src="/qrform.svg" width="200" alt="QRForm"/>
+    <h2>Vos formulaires</h2>
 
-    <h1>UserEdit</h1>
+    {#if loading}
+        <h2>Chargement...</h2>
+        <CircularProgress style="height: 24px; width: 24px;" indeterminate/>
+    {/if}
+    {#if error}
+        <h2>{error}</h2>
+    {/if}
 
+    {#each forms as form}
+        <FormItem form={form}/>
+    {/each}
 </main>
+
+
+<style>
+    main {
+        min-height: 100vh;
+    }
+
+    h4 {
+        color: #000;
+    }
+
+</style>
---
+++ src/lib/form/FormItem.svelte
@@ -0,0 +1,50 @@
+<script>
+    import Icon from '@smui/textfield/icon'
+    import { Link } from 'svelte-routing'
+
+    export let form
+</script>
+
+
+<Link to="">
+    <button class="container">
+        <div class="left">
+            <h4>{form.title}</h4>
+            <span>Destination: {form.email_to}</span>
+            <span>Titre de l'email: {form.email_title}</span>
+        </div>
+
+        <Icon class="material-icons" slot="chevron-right">chevron_right</Icon>
+    </button>
+</Link>
+
+<style>
+    .container {
+        border-radius: 4px;
+        background-color: #e0e0e0;
+        color: #000;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .container:hover {
+        border-radius: 4px;
+        filter: brightness(0.8);
+
+    }
+
+    .left {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    .left span {
+        color: #666;
+    }
+
+    h4 {
+        margin: 0;
+    }
+
+</style>
